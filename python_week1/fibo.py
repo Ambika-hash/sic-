8dbe4cffc46582d40cfe0396696b722e12a01791
Ambika-hash <ambika.m.sharma2005@gmail.com>
2025-06-26 14:26:11 +0530

    assign

--- python_week1/fibo.py
+++ python_week1/fibo.py
@@ -3,12 +3,11 @@ input_n = int(input("Enter the range: "))
 print("The fibonacci series is: ", end = " ")
 if input_n > 2:
     a ,b = 1, 2
-    print(a, b, end = " ")
     for i in range(input_n):
         c = a + b
         a, b = b, c
-        print(c, end = " ")
+    print(c, end = " ")
 elif input_n == 1:
-    print(1)
+    print(input_n)
 else:
-    print("1 2")
+    print(input_n)
