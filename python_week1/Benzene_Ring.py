ab53d862ab6c87543ecfe5f5a52b1b6a832d09d5
Ambika-hash <ambika.m.sharma2005@gmail.com>
2025-06-27 08:58:52 +0530

    assign

--- python_week1/Benzene_Ring.py
+++ python_week1/Benzene_Ring.py
@@ -1,17 +1,18 @@
 #Benzene Ring (C6H6) Hexagon
-n = int(input("Enter the N value (>=3): "))
+# n = int(input("Enter the N value (>=3): "))
+n = 5
 if n < 3:
     print("Please enter a value of N >= 3")
 else:
-    for i in range(n):
+    for i in range(n//2):
         spaces = " " * (n - i - 1)
         if i == 0:
-            print(spaces + "#")
+            print(spaces + "# " * n)
         else:
-            print(spaces + "#" + " " * (2 * (n + i - 1) - 1) + "#")
-    for i in range(n - 2, -1, -1):
+            print(spaces + "# " + " " * (2 * (n + i - 1) - 1) + "# ")
+    for i in range(n//2 + 1, 0, -1):
         spaces = " " * (n - i - 1)
-        if i == 0:
-            print(spaces + "#")
+        if i == 1:
+            print(spaces + " #" * n)
         else:
             print(spaces + "#" + " " * (2 * (n + i - 1) - 1) + "#")
